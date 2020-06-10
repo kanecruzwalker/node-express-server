24a7bc3ea843fbe699ba919fcf188a1e9937c070
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-09 21:35:29 -0700

    created a get route for root directory, with a response proving connectio

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+// variable needed for app
 const express = require("express");
 const app = express();
+
+
+// a get 
+app.get("/", function(request, response){
+    response.send("Hello");
+});
+
+
+
+
+// listener to start the app on a port
 app.listen(3000, function () {
     console.log("Server Started on Port 3000");
 });
